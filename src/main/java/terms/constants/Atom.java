46d3064c9653fbcd361c543e1fb0b0164edeca73
Aidan Scannell <scannell.aidan@gmail.com>
2018-02-06 13:52:00 +0000

    Added atom constructor which sets functor (name) and a isValid function which checks that the first letter is lowercase and returns true/false.

--- src/main/java/terms/constants/Atom.java
+++ src/main/java/terms/constants/Atom.java
@@ -1,4 +1,25 @@
 package main.java.terms.constants;
 
-public class Atom {
+import main.java.terms.Constant;
+
+import static java.lang.Character.*;
+
+/* Represents an atom (a positive literal with no argument or annotation) */
+public class Atom extends Constant{
+
+    // Fields:
+    private String functor;
+
+    // Constructor:
+    public Atom(String functor){
+        this.functor = functor;
+    }
+
+    // Methods:
+
+    // Check atom name is all lowercase
+    public static boolean isValid(String s){
+        char check = s.charAt(0);
+        return isLowerCase(check);
+    }
 }
