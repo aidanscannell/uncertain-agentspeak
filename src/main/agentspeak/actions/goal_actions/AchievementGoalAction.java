af05b6b2a8a8417d9403d0e3f46f996efb55e475
Aidan Scannell <as12528@my.bristol.ac.uk>
2018-07-03 17:04:55 +0100

    Changed internal event to AddGoalET

--- src/main/agentspeak/actions/goal_actions/AchievementGoalAction.java
+++ src/main/agentspeak/actions/goal_actions/AchievementGoalAction.java
@@ -3,6 +3,7 @@ package main.agentspeak.actions.goal_actions;
 import main.agentspeak.*;
 import main.agentspeak.actions.GoalAction;
 import main.agentspeak.event_triggers.GoalEventTrigger;
+import main.agentspeak.event_triggers.goal_event_triggers.AddGoalET;
 import main.agentspeak.events.InternalEvent;
 import main.agentspeak.goals.AchievementGoal;
 
@@ -22,7 +23,7 @@ public class AchievementGoalAction extends GoalAction {
     @Override
     public boolean executeAction(Intention intention, Unifier unifier, BeliefBase beliefBase, EventSet eventSet) {
         AchievementGoal achievementGoalSub = this.achievementGoal.substitute(unifier);
-        eventSet.add(new InternalEvent(new GoalEventTrigger(achievementGoalSub),intention));
+        eventSet.add(new InternalEvent(new AddGoalET(achievementGoalSub),intention));
         System.out.println("Subgoal added: " + achievementGoalSub);
         return true;
     }
