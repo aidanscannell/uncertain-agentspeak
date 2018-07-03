bcf953af88c6b013081ab3bd6d98b9e068288a31
Aidan Scannell <as12528@my.bristol.ac.uk>
2018-07-03 17:05:48 +0100

    Added unify() methods

--- src/main/agentspeak/event_triggers/belief_event_triggers/ReviseBeliefET.java
+++ src/main/agentspeak/event_triggers/belief_event_triggers/ReviseBeliefET.java
@@ -1,7 +1,10 @@
 package main.agentspeak.event_triggers.belief_event_triggers;
 
 import main.agentspeak.Term;
+import main.agentspeak.Unifier;
 import main.agentspeak.event_triggers.BeliefEventTrigger;
+import main.agentspeak.event_triggers.goal_event_triggers.AddGoalET;
+import main.agentspeak.event_triggers.goal_event_triggers.DeleteGoalET;
 import main.agentspeak.logical_expressions.terminals.BeliefLiteral;
 
 public class ReviseBeliefET extends BeliefEventTrigger {
@@ -19,6 +22,26 @@ public class ReviseBeliefET extends BeliefEventTrigger {
 
     public Term getWeight() {
         return weight;
+    }
+
+    public Unifier unify(AddGoalET addGoalET) {
+        return null;
+    }
+
+    public Unifier unify(DeleteGoalET deleteGoalET) {
+        return null;
+    }
+
+    public Unifier unify(ReviseBeliefET reviseBeliefET) {
+        Unifier unifierLiteral = super.getBeliefLiteral().unify(reviseBeliefET.getBeliefLiteral());
+        if (unifierLiteral != null) {
+            Unifier unifierWeight = weight.substitute(unifierLiteral).unify(reviseBeliefET.getWeight().substitute(unifierLiteral));
+            if (unifierWeight != null) {
+                unifierLiteral.putAll(unifierWeight);
+                return unifierLiteral;
+            }
+        }
+        return null;
     }
 
     @Override
--- src/main/agentspeak/event_triggers/goal_event_triggers/DeleteGoalET.java
+++ src/main/agentspeak/event_triggers/goal_event_triggers/DeleteGoalET.java
@@ -1,12 +1,26 @@
 package main.agentspeak.event_triggers.goal_event_triggers;
 
 import main.agentspeak.Goal;
+import main.agentspeak.Unifier;
 import main.agentspeak.event_triggers.GoalEventTrigger;
+import main.agentspeak.event_triggers.belief_event_triggers.ReviseBeliefET;
 
 public class DeleteGoalET extends GoalEventTrigger {
 
     public DeleteGoalET(Goal goal) {
         super(goal);
+    }
+
+    public Unifier unify(AddGoalET addGoalET) {
+        return null;
+    }
+
+    public Unifier unify(DeleteGoalET deleteGoalET) {
+        return super.getGoal().getTerm().unify(deleteGoalET.getGoal().getTerm());
+    }
+
+    public Unifier unify(ReviseBeliefET reviseBeliefET) {
+        return null;
     }
 
     @Override
