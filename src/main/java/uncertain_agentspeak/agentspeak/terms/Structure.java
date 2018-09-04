4822fdcbd0c6373e877f468513bccf3578aebe78
Aidan Scannell <as12528@my.bristol.ac.uk>
2018-09-04 16:16:10 +0100

    Corrected unification for a structure with a structure

--- src/main/java/uncertain_agentspeak/agentspeak/terms/Structure.java
+++ src/main/java/uncertain_agentspeak/agentspeak/terms/Structure.java
@@ -69,25 +69,11 @@ public class Structure extends Term {
                 Term termStruct = struct.getArgument(i);
                 Term termThis = this.getArgument(i);
 
-                if (termStruct instanceof Constant){
-                    if (termThis instanceof Variable) {
-                        Variable var = (Variable) termThis;
-                        unifier.put(var,termStruct);
-                    }
-                } else if (termStruct instanceof Variable){
-                    if (!termStruct.equals(termThis)) {
-                        Variable var = (Variable) termStruct;
-                        unifier.put(var, termThis);
-                    }
-                } else if (termStruct instanceof Structure){
-                    if (termThis instanceof Variable) {
-                        Variable var = (Variable) termThis;
-                        unifier.put(var,termStruct);
-                    } else if (termThis instanceof Structure) {
-//                        Unifier structUnifier = new Unifier();
-                        Unifier structUnifier = this.getArgument(i).unify(struct.getArgument(i));
-                        unifier.putAll(structUnifier);
-                    }
+                Unifier argUnifier = termThis.unify(termStruct, unifier);
+                if (argUnifier != null) {
+                    unifier.putAll(argUnifier);
+                } else {
+                    return null;
                 }
             }
             return unifier;
@@ -152,124 +138,4 @@ public class Structure extends Term {
         }
         return true;
     }
-
-    //    /** Unify two structures if they have the same functor. */
-//    public Unifier unify(Structure struct){
-//        if ( (this.getFunctor() == struct.getFunctor()) && (this.getArity() == struct.getArity()) ){
-//            Unifier unifier = new Unifier();
-//            for (int i = 0; i<this.getArity(); i++){
-//                Term termStruct = struct.getArguments().get(i);
-//                Term termThis = this.getArguments().get(i);
-//
-//                if (termStruct instanceof Constant){
-//                    Constant c = (Constant) termStruct;
-//                    if (termThis instanceof Constant){
-////                        if (termThis.equals(termStruct)){
-////                            return new Unifier();
-////                        }
-////                        return null;
-//                    } else if (termThis instanceof Variable){
-//                        Variable varThis = (Variable) termThis;
-//                        unifier.put(varThis,c);
-//                    } else if (termThis instanceof Structure){
-//                        //null;
-//                    }
-//
-//                } else if (termStruct instanceof Variable){
-//                    Variable varStruct = (Variable) termStruct;
-//                    if (termThis instanceof Constant){
-//                        unifier.put(varStruct,termThis);
-//
-//                    } else if (termThis instanceof Variable){
-//
-//                        if (termThis.equals(termStruct)){
-////                            return new Unifier();
-//                        } else {
-//                            unifier.put(varStruct, termThis);
-//                        }
-//
-//                    } else if (termThis instanceof Structure){
-//                        unifier.put(varStruct, termThis);
-//                    }
-//
-//                } else if (termStruct instanceof Structure){
-//                    Variable structStruct = (Variable) termStruct;
-//                    if (termThis instanceof Constant){
-//                        //null;
-//                    } else if (termThis instanceof Variable){
-//                        Variable varThis = (Variable) termThis;
-//                        unifier.put(varThis,structStruct);
-//                    } else if (termThis instanceof Structure){
-//                        Unifier unifyStruct = this.unify(structStruct);
-//                    }
-//
-//                } else {
-//                    return null;
-//                }
-//            }
-//            return unifier;
-//        } else {
-//            return null;
-//        }
-//    }
-
-//    /** Unify two structures if they have the same functor. */
-//    public Unifier unify(Structure struct){
-//        if ( (this.getFunctor() == struct.getFunctor()) && (this.getArity() == struct.getArity()) ){
-//            Unifier unifier = new Unifier();
-//            for (int i = 0; i<this.getArity(); i++){
-//                Term termStruct = struct.getArguments().get(i);
-//                Term termThis = this.getArguments().get(i);
-//
-//                if (termStruct instanceof Constant){
-//                    if (termThis instanceof Constant){
-////                        if (termThis.equals(termStruct)){
-////                            return new Unifier();
-////                        }
-////                        return null;
-//                    } else if (termThis instanceof Variable){
-//                        unifier.put(termThis,termStruct);
-//                    } else if (termThis instanceof Structure){
-//                        //null;
-//                    }
-//
-//                } else if (termStruct instanceof Variable){
-//                    if (termThis instanceof Constant){
-//                        unifier.put(termStruct,termThis);
-//
-//                    } else if (termThis instanceof Variable){
-//
-//                        if (termThis.equals(termStruct)){
-////                            return new Unifier();
-//                        } else {
-//                            unifier.put(termStruct, termThis);
-//                        }
-//
-//                    } else if (termThis instanceof Structure){
-//                        unifier.put(termStruct, termThis);
-//                    }
-//
-//                } else if (termStruct instanceof Structure){
-//                    if (termThis instanceof Constant){
-//                        //null;
-//                    } else if (termThis instanceof Variable){
-//                        unifier.put(termThis,termStruct);
-//                    } else if (termThis instanceof Structure){
-//
-//                    }
-//
-//                } else {
-//                    return null;
-//                }
-////                Unifier args = this.getArguments().get(i).unify((Term) struct.getArguments().get(i));
-////                if (args != null){
-////                    unifier.put(struct.getArguments().get(i),this.getArguments().get(i));
-////                }
-//            }
-//            return unifier;
-//        } else {
-//            return null;
-//        }
-//    }
-
 }
